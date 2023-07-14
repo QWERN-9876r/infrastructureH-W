455f3136112651ec5bbd8e2ad5b218db8e7a1b98
QWERN-9876r <dimadobriy111@gmail.com>
2023-07-14 13:22:45 +0400

    some kind of commit 2

--- helpersForCommit/checkTestSolution.js
+++ helpersForCommit/checkTestSolution.js
@@ -1,5 +1,5 @@
 const fs = require('node:fs')
 const path = require('node:path')
-const solution = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'testInfo.txt')))
+const solution = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', '.testInfo.txt')))
 
 if ( solution.numFailedTestSuites ) throw new Error('Тесты не прошли!')
